8561404893c2cfe5b56e3ef01fef007d85c52e52
hungluong249 <hung.luong@matocreative.vn>
2018-04-16 13:06:16 +0700

    fixbug n seltest 110418 12h

--- application/views/templates/public_parts/master_footer_view.php
+++ application/views/templates/public_parts/master_footer_view.php
@@ -49,12 +49,14 @@
 
 
 
-<!-- jQuery Input -->
-<script src="<?php echo site_url('assets/js/script.js') ?>"></script>
+
 <!-- Lightbox JS -->
 <script src="<?php echo site_url('assets/lib/lightbox/js/lightbox.js') ?>"></script>
 <!-- Smooth Scroll JS -->
-<!--<script src="--><?php //echo site_url('assets/lib/smoothScroll/jquery.smoothscroll.js') ?><!--"></script>-->
+<!--<script src="--><?php //echo site_url('assets/lib/smoothScroll/smooth-scroll.min.js') ?><!--"></script>-->
+<!-- jQuery Input -->
+<script src="<?php echo site_url('assets/js/script.js') ?>"></script>
+
 
 <script>
    // $(function() {
@@ -72,6 +74,42 @@
            $('.header').css('' , '');
        }
    });
+</script>
+
+<script>
+	$("a[href*='#']").click(function() {
+	    var hash = this.hash;
+        console.log($(hash).height());
+
+    });
+
+
+    // $(document).ready(function(){
+    //     // Add smooth scrolling to all links
+    //     $("header a[href*='#']").on('click', function(event) {
+    //
+    //         // Make sure this.hash has a value before overriding default behavior
+    //         if (this.hash !== "") {
+    //             // Prevent default anchor click behavior
+    //             event.preventDefault();
+    //
+    //             // Store hash
+    //             var hash = this.hash;
+    //
+    //             // Using jQuery's animate() method to add smooth page scroll
+    //             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //             $('html, body').animate({
+		// 			scrollTop: 0,
+    //                 scrollTop: $(hash).offset().top
+    //             }, 800, function(){
+    //
+    //                 // Add hash (#) to URL when done scrolling (default click behavior)
+    //                 window.location.hash = hash;
+    //             });
+    //
+    //         } // End if
+    //     });
+    // });
 </script>
 
 </body>
--- assets/js/script.js
+++ assets/js/script.js
@@ -24,22 +24,44 @@ $(document).ready(function(){
 
     var winWidth = $(window).width();
 
-    if(winWidth <= 768){
+    if(winWidth <= 992){
         $('nav > .nav-expand').hide();
         var i = 1;
 
         $('#nav-expand-btn').click(function(){
             if(i === 1){
                 $('nav > .nav-expand').slideDown();
+                $('.header').css('background' , '#333');
                 i = 0
             } else {
                 $('nav > .nav-expand').slideUp();
+                $('.header').css('background' , '')
                 i = 1
             }
         })
+
+        //Make gallery square size item
+        var maskWidth = $('.container-fluid#gallery .mask').width();
+        //console.log(maskWidth);
+        $('.container-fluid#gallery .mask').css('height' , maskWidth);
+        //var maskHeight = $('.container-fluid#gallery .mask').height();
+        //console.log(maskHeight);
     };
 
 
+    // //scroll js
+    // smoothScroll.init({
+    //     selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+    //     selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    //     speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    //     easing: 'easeInOutCubic', // Easing pattern to use
+    //     updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    //     offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    //     callback: function ( toggle, anchor ) {} // Function to run after scrolling
+    // });
+
 });
+
+
 
 
